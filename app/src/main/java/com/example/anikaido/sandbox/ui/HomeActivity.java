8d12c3a05690ad46bba745f0560ba134781169d7
sabakaido <amane.nikaido@gmail.com>
2017-01-05 22:25:45 +0900

    アバター表示

--- app/src/main/java/com/example/anikaido/sandbox/ui/HomeActivity.java
+++ app/src/main/java/com/example/anikaido/sandbox/ui/HomeActivity.java
@@ -1,18 +1,24 @@
 package com.example.anikaido.sandbox.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.anikaido.sandbox.R;
 import com.example.anikaido.sandbox.ui.helper.HomeActivityHelper;
 import com.facebook.AccessToken;
 import com.facebook.GraphResponse;
 
+import org.json.JSONObject;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import rx.functions.Action1;
 
 /**
@@ -21,7 +27,10 @@ import rx.functions.Action1;
 
 public class HomeActivity extends AppCompatActivity {
     @BindView(R.id.accessTokenTextView)
-    TextView accessTokenTextView;
+    TextView mAccessTokenTextView;
+
+    @BindView(R.id.profile_image)
+    CircleImageView mCircleImageView;
 
     AccessToken mAccessToken;
 
@@ -36,13 +45,26 @@ public class HomeActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         mAccessToken = (AccessToken) intent.getParcelableExtra("accesstoken");
-        accessTokenTextView.setText(mAccessToken.getToken());
+        mAccessTokenTextView.setText(mAccessToken.getToken());
 
         mHomeActivityHelper = new HomeActivityHelper();
+
+        final Activity activity = this;
         mHomeActivityHelper.getProfile(mAccessToken).subscribe(new Action1<GraphResponse>() {
             @Override
             public void call(GraphResponse graphResponse) {
-                accessTokenTextView.setText(graphResponse.toString());
+                mAccessTokenTextView.setText(graphResponse.getRawResponse());
+
+                    try {
+                        JSONObject json = new JSONObject(graphResponse.getRawResponse());
+                        String url = json.getJSONObject("picture").getJSONObject("data").getString("url");
+                        Glide.with(activity)
+                                .load(url)
+                                .fitCenter()
+                                .into(mCircleImageView);
+                    } catch (Exception e) {
+                        Log.d("hoge", e.getMessage());
+                    }
             }
         });
     }
