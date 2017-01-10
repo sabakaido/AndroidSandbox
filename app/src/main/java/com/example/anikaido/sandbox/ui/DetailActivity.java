4d4f36e6943b95d6da0c99d48b31f1bfd77f23a9
sabakaido <amane.nikaido@gmail.com>
2017-01-10 23:00:09 +0900

    CordinatorLayout追加

--- app/src/main/java/com/example/anikaido/sandbox/ui/DetailActivity.java
+++ app/src/main/java/com/example/anikaido/sandbox/ui/DetailActivity.java
@@ -3,17 +3,29 @@ package com.example.anikaido.sandbox.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.example.anikaido.sandbox.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by anikaido on 2017/01/10.
  */
 
 public class DetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.tool_bar)
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        ButterKnife.bind(this);
+
+        mToolbar.setTitle("詳細");
     }
 }
